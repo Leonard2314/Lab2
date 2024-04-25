4571a72bb79b65345ae04f7553cbdffb08cc6248
leonard <leonardsan.23@ichat.sp.edu.sg>
2024-04-25 11:22:19 +0800

    new changes

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,17 @@
-print ("ET0375 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def calculate_bmi(height,weight):
+        
+        print("Height="+ str(height))
+        print("Weight="+ str(weight))
+        bmi= weight/(height*height)
+        print("BMI IS="+str(bmi))
+        if bmi<18.5:
+                print("person is under weight")
+        elif (bmi>=18.5 and bmi<=25):
+                print("person if normal weight")
+        elif(bmi>25.0):
+                print("person is overweight")
+
+
+calculate_bmi(weight = 57, height =1.73)
+
