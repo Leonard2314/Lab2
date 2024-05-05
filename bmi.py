6e1d431869b3d38e2fdde2d456ac20685f064903
leonard <leonardsan.23@ichat.sp.edu.sg>
2024-05-05 19:47:11 +0800

    bmi

---
+++ bmi.py
@@ -0,0 +1,19 @@
+﻿def calculate_bmi(height,weight):
+       
+        print("Height="+ str(height))
+        print("Weight="+str(weight))
+        bmi= weight/(height*height)
+        print("BMI IS="+str(bmi))
+        if bmi<18.5:
+               print("person is under weight")
+               x=-1
+               
+        elif (bmi>=18.5 and bmi<=25):
+                print("person if normal weight")
+                x=0
+                
+        elif(bmi>25.0):
+                print("person is overweight") 
+                x=1
+        return x       
+calculate_bmi(weight = 57, height =1.73)   
