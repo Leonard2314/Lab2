ecf8b9cf596732f7dadf32a9d557117514b31cb1
leonard <leonardsan.23@ichat.sp.edu.sg>
2024-05-05 20:42:48 +0800

    new

--- Lab2.py
+++ Lab2.py
@@ -1,24 +1,4 @@
 import statistics
-def calculate_bmi(height,weight):
-       
-        print("Height="+ str(height))
-        print("Weight="+str(weight))
-        bmi= weight/(height*height)
-        print("BMI IS="+str(bmi))
-        if bmi<18.5:
-               print("person is under weight")
-               x=-1
-               
-        elif (bmi>=18.5 and bmi<=25):
-                print("person if normal weight")
-                x=0
-                
-        elif(bmi>25.0):
-                print("person is overweight") 
-                x=1
-        return x       
-        
-'''
 def display_main_menu():
         print("Enter some numbers separated by commas (e.g. 5, 67,")
 def get_user_input():
@@ -54,5 +34,4 @@ calc_average(z)
 sort_temperature(z)
 find_min_max(z)
 calc_median_temperature(z)
-'''
-calculate_bmi(weight = 57, height =1.73)
+
